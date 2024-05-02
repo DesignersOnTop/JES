0288544e08f604158e73f0511a8f4f6d668b9192
albiery <albieryrf@gmail.com>
2024-05-02 15:30:56 -0400

    'algunas cosas'

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,22 @@ const menu = document.querySelector('.menu');
 
 menu.addEventListener('click',() =>{
     if(menu.classList.toggle("active")) {
-
-    }else{
-
+    Swal.fire({
+        html: '<ul class="menu-navegador-eme"><li class="li-eme"><a class="a-eme">Home</a></li><li class="li-eme"><a class="a-eme">Home</a></li><li class="li-eme"><a class="a-eme">Home</a></li><li class="li-eme"><a class="a-eme">Home</a></li></ul>',
+        backdrop: true,
+        customClass: {
+            popup: 'emergente-class classes-eme',
+            confirmButton: 'confirm-eme',
+            container: 'container-eme conta-eme'
+        },
+        buttonsStyling: false,
+        showCloseButton: false,
+        // closeButtonAriaLabel: "cerrar",
+        allowOutsideClick: true,
+        confirmButtonText: "Aceptar",
+        showConfirmButton: false,
+        position: 'top-right',
+        grow: 'column'
+        });
     }
 });
