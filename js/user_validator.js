b87b101afce8e622fd394a3422d3eb99a7fb30f1
albiery <albieryrf@gmail.com>
2024-05-03 15:02:56 -0400

    'okay'

--- js/user_validator.js
+++ js/user_validator.js
@@ -9,7 +9,42 @@ if(user === "admin" && password =="admin"){
         window.location="e-home.html";
     }else if(user === "profesor" && password === "profesor"){
         window.location = "p-home.html"
-    }else {
-        alert("Usuario No Encontrado, verifique que los datos que este ingresando sean correcto o intente mas tarde")
+    }else if(user === "" && password === "") {
+        Swal.fire({
+            html: '<span class="white">"Por favor, asegúrate de completar todos los campos necesarios para acceder al sistema!"</span',
+            backdrop: true,
+            customClass: {
+                popup: 'emergente-class',
+                confirmButton: 'confirm-eme',
+                container: 'container-eme'
+            },
+            buttonsStyling: false,
+            showCloseButton: false,
+            // closeButtonAriaLabel: "cerrar",
+            allowOutsideClick: false,
+            confirmButtonText: "Aceptar",
+            imageUrl: './imagenes/recursos/logo-jes.png',
+            imageWidth: '140px',
+            imageHeight: '120px'
+        });
+    }
+    else {
+        Swal.fire({
+            html: '<span class="white">"Lo sentimos, parece que el ID o la contraseña ingresados son incorrectos. Por favor, inténtalo de nuevo."</span',
+            backdrop: true,
+            customClass: {
+                popup: 'emergente-class',
+                confirmButton: 'confirm-eme',
+                container: 'container-eme',
+            },
+            buttonsStyling: false,
+            showCloseButton: false,
+            // closeButtonAriaLabel: "cerrar",
+            allowOutsideClick: false,
+            confirmButtonText: "Aceptar",
+            imageUrl: './imagenes/recursos/logo-jes.png',
+            imageWidth: '140px',
+            imageHeight: '120px'
+        });
     }
 }   
